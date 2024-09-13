672c71e68807934753ff584a69dd7d5fe5409a8b
Chori <yidodeveloper@gmail.com>
2024-09-13 01:49:27 +0000

    Feat: 서버에 요청을 보내는 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import Navbars from "../components/Navbars";
+import useAxios from "../hooks/useAxios";
 
 function Home() {
+  const [questions, setQuestions] = useState(null);
+
+  const {responseData, error, isLoading, request} = useAxios({
+    method: "POST",
+    url: `api/result/`,
+  })
+
+  const fetchQuestions = () => {
+    request()
+  }
+
   return (
     <>
       <Navbars />
       <div>
-        <p>홈 화면</p>
+        <p>검사를 시작하시겠습니까?</p>
+        <Button onClick={fetchQuestions}>시작하기</Button>
       </div>
     </>
   );

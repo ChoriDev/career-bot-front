2dfd2751d88cfa0a2260d020470c038f1857862b
Chori <yidodeveloper@gmail.com>
2024-10-22 19:47:43 +0900

    ✨ 검사 결과를 받아서 화면에 출력

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,7 +1,62 @@
+import { useEffect } from "react";
+import useAxios from "../hooks/useAxios";
+import { Card, Table } from "react-bootstrap";
+import Navbars from "../components/Navbars";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import styles from "../css/Result.module.css";
+
 function Result() {
+  const studentId = localStorage.getItem("student-id");
+
+  const { responseData, error, isLoading, request } = useAxios({
+    method: "GET",
+    url: `api/students/${studentId}/result/`,
+  });
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
     <>
-      <div>this is result page</div>
+      <Navbars />
+      <Card className={styles.titleCard}>
+        <Card.Body>검사 결과</Card.Body>
+      </Card>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div className={styles.resultBox}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>진로 성숙도</th>
+              </tr>
+            </thead>
+            <tbody>
+              {responseData &&
+                Object.entries(responseData).map(
+                  ([category, maturity], index) => (
+                    <tr key={index}>
+                      <td>{category}</td>
+                      <td className={styles.progressBarBox}>
+                        <CircularProgressbar
+                          maxValue={3}
+                          value={maturity.toFixed(2)}
+                          text={maturity.toFixed(2)}
+                        />
+                      </td>
+                    </tr>
+                  )
+                )}
+            </tbody>
+          </Table>
+        </div>
+      )}
     </>
   );
 }
